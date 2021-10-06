f79cccbcbb241607ae1e66b574f81e2a905a1437
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-06 10:11:27 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+/*
+To generate a random number between a and b keep in mind this formula:
+a-> minimum value 
+b->maximum value */
+let randomNumber = a+(b-a)*Math.random();
+/*this gives you any random number between a and b
+*/
